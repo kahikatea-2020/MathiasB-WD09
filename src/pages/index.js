a164acd55f8283e6de528e6a1df86a9654adab51
Mathias Bast <mathiasbast123@gmail.com>
2020-04-23 18:44:32 +1200

    inital commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,14 +7,14 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Home</h1>
+    <h2>Hey my name is Mathias and I made this beautiful website</h2>
+    <p>Did you know there are over 30 days in a year and over 12 seconds in a minute</p>
+    <p>Math tests be like: Emma had 12 Apples and John took 3, What is the distance between the sun squared in factorials ¯\_(ツ)_/¯</p>
+    <Link to='page-2' >Want to see the first few digits of pi?</Link>
+    <br />
+    <img src='/FatBastard.jpg' />
+    <p>I had dinner like 15 minutes ago and I could not rember what I had for the life of me. It is like it never happend</p>
   </Layout>
 )
 
